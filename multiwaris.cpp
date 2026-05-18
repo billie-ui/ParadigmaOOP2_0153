d6a9c3d84040cad3fe3b812a52aa4f94675e2c9d
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-18 14:57:23 +0700

    class orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class orang
+{
+    public:
+        int umur;
+        orang (int pUmur) :
+        umur (pUmur)
+    
+    {
+        cout << "Orang Dibuat Dengan Umur " << umur << "\n" << endl;
+    }
+};
